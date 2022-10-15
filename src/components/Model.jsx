b2d7fa8510c3eccc7f150e81ab91d09858524783
mgokulraj <mgokulraj.uk@gmail.com>
2022-10-16 00:49:26 +0100

    Before Final

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,16 +2,43 @@ import React from "react";
 import { createPortal } from "react-dom";
 import "./Model.css";
 
-const Model = (props) => {
+const Model = ({ closeModal }) => {
   const modelContainer = document.getElementById("model");
 
-  console.log(modelContainer);
   return createPortal(
-    <div className="model-overlay">
-      <div className="model-container">
-        <section className="upper-section"></section>
-        <section className="middel-section"></section>
-        <section className="lower-section"></section>
+    <div onClick={closeModal} className="modal-overlay">
+      <div onClick={(e) => e.stopPropagation()} className="modal-container">
+        <div className="upper-section">
+          <h1>Huge Annoucement!</h1>
+          <p onClick={closeModal} className="close">
+            &times;
+          </p>
+        </div>
+        <div className="middle-section">
+          <h3>React Mega Tutorial is LIVE!</h3>
+          <p>Do you wish to learn React?</p>
+          <p>
+            I've published a completely FREE react tutorial, check it out today!
+          </p>
+          <p>
+            I bring to you this mega course where each month I will help you
+            learn about these libraries and frameworks by building 1 cool
+            project.
+          </p>
+        </div>
+        <div className="lower-section">
+          <a onClick={closeModal} className="modal-btn btn-red">
+            Close
+          </a>
+          <a
+            href="http://theleanprogrammer.com/aam/"
+            target="_blank"
+            rel="noreferrer"
+            className="modal-btn btn-green"
+          >
+            Start tutorial
+          </a>
+        </div>
       </div>
     </div>,
     modelContainer
