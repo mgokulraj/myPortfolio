1f8d4528737784b1f9275369cd234edd59091eaa
mgokulraj <mgokulraj.uk@gmail.com>
2022-10-16 17:16:50 +0100

    Final before profile link

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -2,7 +2,7 @@ export const projectsData = [
   {
     projectName: "Food Search",
     projectDescription:
-      "Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!",
+      "Check out this cool food search application which will help you find the best food recipies with ingredients and servings. Also you can add your own recipies using API key. This application is built with forkify API.",
     imageUrl:
       "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/proj1.PNG",
     projectUrl: "https://food-search-v2.netlify.app/",
@@ -10,7 +10,7 @@ export const projectsData = [
   {
     projectName: "Workout Monitor",
     projectDescription:
-      "Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.",
+      "Here is the workout monitor application which you can add your running and cycling workouts and monitor day to day. Also it will convert to minutes per KM and SPM based on the input given by you.",
     imageUrl:
       "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/proj2.PNG",
     projectUrl: "https://workout-myapp.netlify.app/",
@@ -18,7 +18,7 @@ export const projectsData = [
   {
     projectName: "Bank Transaction",
     projectDescription:
-      "A frontend web developer must know the concepts of CSS Flexbox. So here is a CSS Flexbox cheatsheet, built using CSS Flexbox. Learn the concepts of flexbox by building a cheatsheet which is built using flexbox. Super cool!",
+      "A simple bank transaction application which you can see the deposits and withdrawls. Also you can request the loan amount and close the account online.",
     imageUrl:
       "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/proj3.PNG",
     projectUrl: "https://bankist-myapp.netlify.app/",
@@ -26,7 +26,7 @@ export const projectsData = [
   {
     projectName: "Dice shuffle game",
     projectDescription:
-      "Pokemon was a huge part of our childhood, wasn't it? Talking about pokemon brings back so many memories, but do you remember the details of each pokemon? Probably No! So here I bring you a pokedex to check all pokemons :)",
+      "Checkout the dice shuffle game, play by clicking the roll dice button and get the points. Its 2 players game and the one gets the 100 points first win the game :)",
     imageUrl:
       "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/proj4.PNG",
     projectUrl: "https://diceshuffle-game.netlify.app/",
@@ -34,7 +34,7 @@ export const projectsData = [
   {
     projectName: "Portfolio Website",
     projectDescription:
-      "As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.",
+      "As a front-end developer, you should definitely have your own portfolio website. There are endless benefits through this portfolio, here is mine",
     imageUrl:
       "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/proj5.PNG",
     projectUrl: "http://madhavbahl.tech/portfolio/",
