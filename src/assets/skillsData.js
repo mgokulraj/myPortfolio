b58656719172fee85eba2d994d125add5878a983
mgokulraj <mgokulraj.uk@gmail.com>
2022-10-16 12:32:48 +0100

    after logos

--- src/assets/skillsData.js
+++ src/assets/skillsData.js
@@ -18,21 +18,21 @@ export const skillList = [
   {
     skillName: "TailwindCSS",
     skillUrl:
-      "https://raw.githubusercontent.com/mgokulraj/LogosAndVectors/main/logos-vectors/Tailwind-logo-new.png?token=GHSAT0AAAAAABZLJ3R4ROI7UOIPEY5WCFS6Y2L37YA",
+      "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/Tailwind-logo-new.png",
   },
   {
     skillName: "Azure",
     skillUrl:
-      "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/Azure_logo.png",
+      "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/Azure-logo.png",
   },
   {
     skillName: "Windows",
     skillUrl:
-      "https://raw.githubusercontent.com/mgokulraj/LogosAndVectors/main/logos-vectors/Windows-round.png?token=GHSAT0AAAAAABZLJ3R4RHDWNMSEWHU6EBPCY2L4IEA",
+      "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/Windows-round.png",
   },
   {
     skillName: "VMWare",
     skillUrl:
-      "https://raw.githubusercontent.com/mgokulraj/LogosAndVectors/main/logos-vectors/VMware-black.png?token=GHSAT0AAAAAABZLJ3R5CCXJS5OCIJQBQMCYY2L5EKQ",
+      "https://raw.githubusercontent.com/mgokulraj/myPortfolio/master/src/assets/VMware-black.png",
   },
 ];
